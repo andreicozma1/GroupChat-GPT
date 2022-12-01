15155df65d74040f39bc8555255c7ac396405d44
Andrei Cozma <me@andreicozma.com>
2022-11-30 22:02:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AI-Chatbot',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

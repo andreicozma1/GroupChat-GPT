1a660e60e0f09e0f5b098abde98e0b05516e9d4b
Andrei Cozma <me@andreicozma.com>
2022-11-30 22:56:08 -0500

    Quick update/fix

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -5,25 +5,82 @@
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <!--      <ExploreContainer name="Tab 1 page"/>-->
+
+
+      <ion-list>
+        <ion-item v-for="(item, index) in items" :key="index">
+          <ion-avatar slot="start">
+            <img :src="'https://picsum.photos/80/80?random=' + index" alt="avatar"/>
+          </ion-avatar>
+          <ion-label>{{ item }}</ion-label>
+        </ion-item>
+      </ion-list>
+      <ion-infinite-scroll @ionInfinite="ionInfinite">
+        <ion-infinite-scroll-content></ion-infinite-scroll-content>
+      </ion-infinite-scroll>
+
+
     </ion-content>
+
+    <ion-card>
+      <ion-card-header>
+        <ion-card-title>AI Chatbot</ion-card-title>
+        <!--            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>-->
+      </ion-card-header>
+
+      <ion-card-content>
+        <ion-textarea
+            :value="message"
+            auto-grow
+            autofocus
+            clear-input
+            enterkeyhint="send"
+            inputmode="text"
+            minlength="1"
+            placeholder="Type to chat with the AI"
+            required
+            spellcheck
+            style="border: 1px solid lightblue; border-radius: 5px; padding: 5px;"
+            type="text"
+        />
+      </ion-card-content>
+      <ion-button class="ion-margin" fill="outline" @click="sendMessage">Send</ion-button>
+
+    </ion-card>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts" setup>
+import { reactive, ref } from "vue"
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/vue"
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+const message = ref("Hello, how are you today?")
+const items = reactive([]) as string[]
+
+const generateItems = () => {
+  const count = items.length + 1
+  for (let i = 0; i < 50; i++) {
+    items.push(`Item ${count + i}`)
+  }
+}
+
+const ionInfinite = (ev) => {
+  generateItems()
+  setTimeout(() => ev.target.complete(), 500)
+}
+
+const sendMessage = () => {
+  console.log("Hello World")
+}
+
+generateItems()
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,45 +2,9 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>AI Chatbot</ion-card-title>
-          <!--            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>-->
-        </ion-card-header>
-
-        <ion-card-content>
-          <ion-textarea
-              :value="message"
-              auto-grow
-              autofocus
-              clear-input
-              enterkeyhint="send"
-              inputmode="text"
-              minlength="1"
-              placeholder="Type to chat with the AI"
-              required
-              spellcheck
-              style="border: 1px solid lightblue; border-radius: 5px; padding: 5px;"
-              type="text"
-          />
-        </ion-card-content>
-        <ion-button class="ion-margin" fill="outline" @click="sendMessage">Send</ion-button>
-
-      </ion-card>
-
-
     </ion-tabs>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-import { IonPage, IonRouterOutlet, IonTabs } from "@ionic/vue"
-import { ref } from "vue"
-
-const message = ref("Hello, how are you today?")
-
-const sendMessage = () => {
-  console.log("Hello World")
-}
-</script>
+import { IonPage, IonRouterOutlet, IonTabs } from "@ionic/vue"</script>

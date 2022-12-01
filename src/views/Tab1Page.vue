f95d6bdd489fad0d348999e79cdf3a4e4c7c6b91
Andrei Cozma <me@andreicozma.com>
2022-12-01 01:51:59 -0500

    Quick update/fix

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -73,7 +73,7 @@
 
     <ion-card>
       <ion-card-header>
-        <ion-card-title>AI Chatbot</ion-card-title>
+        <ion-card-title>AI Chat-bot</ion-card-title>
         <!--            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>-->
       </ion-card-header>
 

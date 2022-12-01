dbdccf1796c8730210ba7928a95c4672aa1acc19
Andrei Cozma <me@andreicozma.com>
2022-11-30 23:29:05 -0500

    Added message feed

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -15,21 +15,38 @@
 
       <!--      <ExploreContainer name="Tab 1 page"/>-->
 
+      <ion-content>
+        <ion-list lines="full">
+          <ion-item v-for="(item, index) in items" :key="index">
+            <!--          A chat style where the user's bubbles are on the right side, and the AI's bubbles are on the left side in a different color -->
+            <ion-avatar :slot="item.type === 'user' ? 'end' : 'start'" edge
+                        style="margin: 0">
+              <img :src="item.avatar" alt="avatar"/>
+            </ion-avatar>
+            <!--          Set margin to push to left or right -->
+            <ion-card
+                :color="item.type === 'user' ? 'light' : 'secondary'"
+                :style="item.type === 'user' ? {marginLeft: 'auto'} : {marginRight: 'auto'}">
 
-      <ion-list>
-        <ion-item v-for="(item, index) in items" :key="index">
-          <ion-avatar slot="start">
-            <img :src="'https://picsum.photos/80/80?random=' + index" alt="avatar"/>
-          </ion-avatar>
-          <ion-label>{{ item }}</ion-label>
-        </ion-item>
-      </ion-list>
-      <ion-infinite-scroll @ionInfinite="ionInfinite">
-        <ion-infinite-scroll-content></ion-infinite-scroll-content>
-      </ion-infinite-scroll>
+              <ion-card-header>
+                <ion-card-subtitle>{{ item.title }} @ {{ item.time }}</ion-card-subtitle>
+              </ion-card-header>
 
+              <div style="height: 1px; margin: 0 10px; background-color: #000000"></div>
 
+              <ion-card-content>
+                {{ item.message }}
+              </ion-card-content>
+
+            </ion-card>
+          </ion-item>
+        </ion-list>
+        <ion-infinite-scroll @ionInfinite="ionInfinite">
+          <ion-infinite-scroll-content></ion-infinite-scroll-content>
+        </ion-infinite-scroll>
+      </ion-content>
     </ion-content>
+
 
     <ion-card>
       <ion-card-header>
@@ -64,12 +81,34 @@ import { reactive, ref } from "vue"
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/vue"
 
 const message = ref("Hello, how are you today?")
-const items = reactive([]) as string[]
+const items = reactive([])
+
+const users = {
+  user: {
+    name  : "User",
+    avatar: "https://picsum.photos/80/80?random=1"
+  },
+  bot : {
+    name  : "Bot",
+    avatar: "https://picsum.photos/80/80?random=2"
+  }
+}
 
 const generateItems = () => {
   const count = items.length + 1
   for (let i = 0; i < 50; i++) {
-    items.push(`Item ${count + i}`)
+    const ci = count + i
+    // alternate between user and bot
+    const type = i % 2 === 0 ? "user" : "bot"
+    const actor = users[type]
+    items.push({
+      type   : type,
+      name   : actor.name,
+      avatar : actor.avatar,
+      title  : "Title",
+      message: "Message " + ci,
+      time   : new Date().toLocaleTimeString()
+    })
   }
 }
 

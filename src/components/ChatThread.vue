00a109c576e46d2acc505c3e6b56b633edde8bdf
Andrei Cozma <me@andreicozma.com>
2022-12-21 23:39:42 -0600

    Quick update/fix

--- src/components/ChatThread.vue
+++ src/components/ChatThread.vue
@@ -1,20 +1,20 @@
 <template>
   <q-scroll-area ref="threadElem" :style="getScrollAreaStyle">
     <q-chat-message
-      class="q-pt-md"
-      :label="threadMessages.length.toString() + ' messages'"
+        class="q-pt-md"
+        :label="threadMessages.length.toString() + ' messages'"
     />
     <div v-for="message in threadMessages" :key="message.date">
       <q-chat-message
-        v-bind="message"
-        size="6"
-        :bg-color="message.sent ? null : getSeededQColor(message.name, 1, 2)"
-        @click="copyMessage(message)"
+          v-bind="message"
+          size="6"
+          :bg-color="message.sent ? null : getSeededQColor(message.name, 1, 2)"
+          @click="copyMessage(message)"
       >
         <div v-for="text in message.text" :key="text">
-          <div v-for="line in text.split('\n')" :key="line">
+          <div v-for="line in getSplitText(text)" :key="line">
             {{ line }}
-            <br />
+            <br/>
           </div>
           <q-tooltip v-if="message.dateCreated" :delay="750">
             {{ createHoverHint(message) }}
@@ -22,15 +22,15 @@
         </div>
 
         <template v-if="message.loading">
-          <q-spinner-dots class="q-ml-md" color="primary" size="1.5em" />
+          <q-spinner-dots class="q-ml-md" color="primary" size="1.5em"/>
         </template>
 
         <template v-slot:stamp>
           <div class="row items-center">
             <span>
               <q-icon
-                :name="getObjectiveIcon(message.objective)"
-                class="q-mr-sm"
+                  :name="getObjectiveIcon(message.objective)"
+                  class="q-mr-sm"
               />
               <q-tooltip v-if="promptTypes[message.objective]?.config">
                 Objective: {{ message.objective }}
@@ -42,9 +42,9 @@
                 {{ dateToStr(message.date) }}
               </q-tooltip>
             </span>
-            <q-space />
+            <q-space/>
             <span v-if="message.cached">
-              <q-icon name="cached" class="q-ml-sm" />
+              <q-icon name="cached" class="q-ml-sm"/>
               <q-tooltip v-if="message.dateCreated">
                 Generated
                 {{ getTimeAgo(message.dateCreated) }}
@@ -56,18 +56,18 @@
 
         <div v-if="message.images.length > 0">
           <q-card
-            v-for="image in message.images"
-            class="bg-grey-1"
-            :key="image"
-            :title="image"
-            flat
+              v-for="image in message.images"
+              class="bg-grey-1"
+              :key="image"
+              :title="image"
+              flat
           >
             <q-card-section class="q-pa-none">
               <q-img
-                draggable
-                :src="image"
-                fit="contain"
-                style="max-height: 400px"
+                  draggable
+                  :src="image"
+                  fit="contain"
+                  style="max-height: 400px"
               />
             </q-card-section>
           </q-card>
@@ -78,112 +78,111 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from "vue";
-import { getSeededQColor } from "src/util/ColorUtils";
-import { promptTypes, TextMessage, useCompStore } from "stores/compStore";
-import { dateToStr, getTimeAgo, smartNotify } from "src/util/Util";
-import { copyToClipboard } from "quasar";
+import { computed, ref, watch } from "vue"
+import { getSeededQColor } from "src/util/ColorUtils"
+import { promptTypes, TextMessage, useCompStore } from "stores/compStore"
+import { dateToStr, getTimeAgo, smartNotify } from "src/util/Util"
+import { copyToClipboard } from "quasar"
 
-const threadElem: any = ref(null);
-const comp = useCompStore();
+const threadElem: any = ref(null)
+const comp = useCompStore()
 
 const props = defineProps({
-  myName: {
-    type: String,
+  myName         : {
+    type    : String,
     required: false,
-    default: "You",
+    default : "You"
   },
   scrollAreaStyle: {
-    type: Object,
+    type    : Object,
     required: false,
-    default: null,
-  },
-});
+    default : null
+  }
+})
 
 const getObjectiveIcon = (objective: string) => {
-  if (!promptTypes[objective]) return "send";
-  if (!promptTypes[objective].icon) return "help";
-  return promptTypes[objective].icon;
-};
+  if (!promptTypes[objective]) return "send"
+  if (!promptTypes[objective].icon) return "help"
+  return promptTypes[objective].icon
+}
 
 const threadMessages = computed(() => {
   const thrd = comp.getThread.messages.map((message: TextMessage) => {
-    const text = message.text.length === 0 ? [] : [...message.text];
-    if (!message.loading && text.length === 0) text.push("[No message]");
+    const text = message.text.length === 0 ? [] : [ ...message.text ]
+    if (!message.loading && text.length === 0) text.push("[No message]")
     return {
       ...message,
-      text: text,
+      text : text,
       stamp: createStamp(message),
-      sent: isSentByMe(message),
-    };
-  });
+      sent : isSentByMe(message)
+    }
+  })
   thrd.sort((a, b) => {
-    const ad = new Date(a.date);
-    const bd = new Date(b.date);
-    return ad.getTime() - bd.getTime();
-  });
-  return thrd;
-});
+    const ad = new Date(a.date)
+    const bd = new Date(b.date)
+    return ad.getTime() - bd.getTime()
+  })
+  return thrd
+})
 
 const createStamp = (message: TextMessage) => {
-  const timeAgo = getTimeAgo(message.date);
-  const sentByMe = isSentByMe(message);
-  return sentByMe ? `Sent ${timeAgo}` : `Received ${timeAgo}`;
-};
+  const timeAgo = getTimeAgo(message.date)
+  const sentByMe = isSentByMe(message)
+  return sentByMe ? `Sent ${timeAgo}` : `Received ${timeAgo}`
+}
 
 const createHoverHint = (message: TextMessage) => {
-  const numText = message.text?.length ?? 0;
-  const numImage = message.images?.length ?? 0;
-  const numTotal = numText + numImage;
-  const who = isSentByMe(message) ? "You" : message.name;
-  const what = `${numTotal} message${
-    numTotal > 1 ? "s" : ""
-  } (${numText} text, ${numImage} image${numImage > 1 ? "s" : ""})`;
-  const when = dateToStr(message.date);
-  return `${who} sent ${what} on ${when}`;
-};
+  const numText = message.text?.length ?? 0
+  const numImage = message.images?.length ?? 0
+  const numTotal = numText + numImage
+  const who = isSentByMe(message) ? "You" : message.name
+  const what = `${numTotal} message${numTotal > 1 ? "s" : ""} (${numText} text, ${numImage} image${numImage > 1 ? "s"
+      : ""})`
+  const when = dateToStr(message.date)
+  return `${who} sent ${what} on ${when}`
+}
 
 const isSentByMe = (message: TextMessage) => {
-  return message.name === props.myName;
-};
+  return message.name === props.myName
+}
 
 const getScrollAreaStyle = computed(() => {
-  const propStyle = props.scrollAreaStyle ? props.scrollAreaStyle : {};
+  const propStyle = props.scrollAreaStyle ? props.scrollAreaStyle : {}
   const defaults = {
-    position: "absolute",
-    left: "0px",
-    right: "0px",
-    top: "50px",
-    bottom: "0px",
-    paddingLeft: "5vw",
-    paddingRight: "5vw",
-  };
+    position    : "absolute",
+    left        : "0px",
+    right       : "0px",
+    top         : "50px",
+    bottom      : "0px",
+    paddingLeft : "5vw",
+    paddingRight: "5vw"
+  }
   return {
-    ...defaults,
-    ...propStyle,
-  };
-});
+    ...defaults, ...propStyle
+  }
+})
 
 const scrollToBottom = (duration?: number) => {
   if (threadElem.value) {
-    duration = duration ?? 750;
+    duration = duration ?? 750
     // scroll to bottom. The element is q-scroll-area
-    const size = threadElem.value.getScroll().verticalSize;
-    threadElem.value.setScrollPosition("vertical", size, duration);
+    const size = threadElem.value.getScroll().verticalSize
+    threadElem.value.setScrollPosition("vertical", size, duration)
   }
-};
+}
+
+const getSplitText = (text: string) => {
+  return text?.split("\n") ?? [ "[ERR: Text is null]" ]
+}
 
 const copyMessage = (message: TextMessage) => {
-  const text = message.text.join("\n");
+  const text = message.text.join("\n")
   copyToClipboard(text).then(() => {
-    smartNotify(`Copied message to clipboard`);
-  });
-};
+    smartNotify(`Copied message to clipboard`)
+  })
+}
 
-watch(
-  () => props.scrollAreaStyle,
-  () => {
-    scrollToBottom(1000);
-  }
-);
+watch(() => props.scrollAreaStyle, () => {
+  scrollToBottom(1000)
+})
 </script>
--- src/stores/compStore.ts
+++ src/stores/compStore.ts
@@ -1,289 +1,276 @@
-import { defineStore } from "pinia";
-import { LocalStorage } from "quasar";
-import { OpenAIApi } from "openai";
-import { CreateCompletionRequest, CreateImageRequest } from "openai/api";
-import { v4 as uuidv4 } from "uuid";
+import { defineStore } from "pinia"
+import { LocalStorage } from "quasar"
+import { OpenAIApi } from "openai"
+import { CreateCompletionRequest, CreateImageRequest } from "openai/api"
+import { v4 as uuidv4 } from "uuid"
 
 interface PromptType {
-  key: string;
-  name?: string;
-  createPrompt: any;
-  config: CreateCompletionRequest | CreateImageRequest;
-  createComp: any;
-  icon: string;
+	key: string;
+	name?: string;
+	createPrompt: any;
+	config: CreateCompletionRequest | CreateImageRequest;
+	createComp: any;
+	icon: string;
 }
 
 export interface GenConfig {
-  promptType: PromptType;
-  maxHistoryLen?: number;
-  ignoreCache: boolean;
+	promptType: PromptType;
+	maxHistoryLen?: number;
+	ignoreCache: boolean;
 }
 
 export interface MessageThread {
-  messages: TextMessage[];
+	messages: TextMessage[];
 }
 
 export interface TextMessage {
-  id?: string | number;
-  text: string[];
-  images: string[];
-  avatar: string;
-  name: string;
-  date?: string | number | Date;
-  objective?: string;
-  dateCreated?: string | number;
-  cached?: boolean;
-  loading?: boolean;
+	id?: string | number;
+	text: string[];
+	images: string[];
+	avatar: string;
+	name: string;
+	date?: string | number | Date;
+	objective?: string;
+	dateCreated?: string | number;
+	cached?: boolean;
+	loading?: boolean;
 }
 
 const openAiConfig = {
-  apiKey: process.env.OPENAI_API_KEY,
-};
+	apiKey: process.env.OPENAI_API_KEY
+}
 
-const openai = new OpenAIApi(openAiConfig);
+const openai = new OpenAIApi(openAiConfig)
 const options = {
-  headers: {
-    Authorization: `Bearer ${openAiConfig.apiKey}`,
-  },
-};
+	headers: {
+		Authorization: `Bearer ${openAiConfig.apiKey}`
+	}
+}
 
 const createChatStartPrompt = (messages: TextMessage[]) => {
-  let res = "The following is a conversation with an AI assistant.";
-  res += "The assistant is helpful, creative, clever, and very friendly.";
-  res +=
-    "When specifically asked to create an image, the assistant will ask it's friend DALL-E to help create an image.";
-  res += "### Human:\nHello, who are you?\n\n";
-  res +=
-    "### Davinci:\nI am an AI created by OpenAI. How can I help you today?\n\n";
-  const maxLength = 10;
+	let res = "The following is a conversation with an AI assistant."
+	res += "The assistant is helpful, creative, clever, and very friendly."
+	res += "When specifically asked to create an image, the assistant will ask it's friend DALL-E to help create an image."
+	res += "### Human:\nHello, who are you?\n\n"
+	res += "### Davinci:\nI am an AI created by OpenAI. How can I help you today?\n\n"
+	const maxLength = 10
 
-  let prompt = messages.map((message) => {
-    const txts = message.text.map((txt) => txt.trim()).join("\n");
-    // let chunk = `### ${message.name.trim()}`
-    let chunk = `### ${message.name.trim()}:`;
-    // const obj = message.objective?.trim()
-    // if (obj) chunk += ` (${obj})`
-    if (txts.trim().length === 0) return chunk;
-    chunk += `\n${txts}`;
-    return chunk;
-  });
-  prompt = prompt.filter((chunk) => chunk !== undefined);
-  prompt = prompt.slice(-maxLength);
-  res += prompt.join("\n\n");
-  return res.trim();
-};
+	let prompt = messages.map((message) => {
+		const txts = message.text.map((txt) => txt.trim()).join("\n")
+		// let chunk = `### ${message.name.trim()}`
+		let chunk = `### ${message.name.trim()}:`
+		// const obj = message.objective?.trim()
+		// if (obj) chunk += ` (${obj})`
+		if (txts.trim().length === 0) return chunk
+		chunk += `\n${txts}`
+		return chunk
+	})
+	prompt = prompt.filter((chunk) => chunk !== undefined)
+	prompt = prompt.slice(-maxLength)
+	res += prompt.join("\n\n")
+	return res.trim()
+}
 
-const createClassificationPrompt = (message: TextMessage[]) => {
-  // create some example prompts
-  let res = "Categorize what to do next into one of the following categories: ";
-  res += "generate_image, none. If you are unsure, choose none.\n";
-  res += "Lastly, create a suitable prompt for the next message.\n\n";
+const createClassificationPrompt = (messages: TextMessage[]) => {
+	// create some example prompts
+	let res = "Categorize what to do next into one of the following categories: "
+	res += "generate_image, none. If you are unsure, choose none.\n"
+	res += "Lastly, create a suitable prompt for the next message.\n\n"
 
-  res += "###\n";
-  res +=
-    "Chat: I can ask my friend DALL-E to help create an image for you. Could you generate a drawing of a dog?" +
-    " It's wearing a space suit and floating in space.\n";
-  res += "Task: generate_image\n";
-  res +=
-    "Prompt: A drawing of a dog wearing a space suit and floating in space.\n\n";
+	res += "###\n"
+	res += "Chat: I can ask my friend DALL-E to help create an image for you. Could you generate a drawing of a dog?" + " It's wearing a space suit and floating in space.\n"
+	res += "Task: generate_image\n"
+	res += "Prompt: A drawing of a dog wearing a space suit and floating in space.\n\n"
 
-  // last 10 messages from the user
-  const lastMessages = message.slice(-10);
-  const prompt = lastMessages.map((m) => m.text.join(". ")).join(". ");
-  res += `\nChat: ${prompt}.`;
-  res += "\nTask:";
-  return res.trim();
-};
+	messages = messages.filter((m) => m.name === "You").slice(-10)
+	const prompt = messages.map((m) => m.text.join(". ")).join(". ")
+	res += `\nChat: ${prompt}.`
+	res += "\nTask:"
+	return res.trim()
+}
 
 const createImagePrompt = (messages: TextMessage[]) => {
-  const lastMessage = messages[messages.length - 1];
-  const lastText = lastMessage.text[lastMessage.text.length - 1];
-  return lastText;
-};
+	const lastMessage = messages[messages.length - 1]
+	const lastText = lastMessage.text[lastMessage.text.length - 1]
+	return lastText
+}
 
 export const promptTypes: Record<string, PromptType> = {
-  chat: {
-    key: "chat",
-    name: "Davinci",
-    config: {
-      model: "text-davinci-003",
-      max_tokens: 250,
-      temperature: 0.75,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: ["###"],
-    },
-    createPrompt: createChatStartPrompt,
-    createComp: openai.createCompletion,
-    icon: "chat",
-  },
-  classify_req: {
-    key: "classify_req",
-    config: {
-      model: "text-davinci-003",
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: ["###"],
-    },
-    createPrompt: createClassificationPrompt,
-    createComp: openai.createCompletion,
-    icon: "question_answer",
-  },
-  generate_image: {
-    key: "generate_image",
-    name: "DALL-E",
-    config: {
-      n: 1,
-      size: "256x256",
-      prompt: "A cute puppy",
-    },
-    createPrompt: createImagePrompt,
-    createComp: openai.createImage,
-    icon: "image",
-  },
-};
+	chat          : {
+		key         : "chat",
+		name        : "Davinci",
+		config      : {
+			model            : "text-davinci-003",
+			max_tokens       : 250,
+			temperature      : 0.75,
+			top_p            : 1,
+			frequency_penalty: 0,
+			presence_penalty : 0,
+			stop             : [ "###" ]
+		},
+		createPrompt: createChatStartPrompt,
+		createComp  : openai.createCompletion,
+		icon        : "chat"
+	},
+	classify_req  : {
+		key         : "classify_req",
+		config      : {
+			model            : "text-davinci-003",
+			temperature      : 0.3,
+			max_tokens       : 100,
+			top_p            : 1,
+			frequency_penalty: 0,
+			presence_penalty : 0,
+			stop             : [ "###" ]
+		},
+		createPrompt: createClassificationPrompt,
+		createComp  : openai.createCompletion,
+		icon        : "question_answer"
+	},
+	generate_image: {
+		key         : "generate_image",
+		name        : "DALL-E",
+		config      : {
+			n     : 1,
+			size  : "256x256",
+			prompt: "A cute puppy"
+		},
+		createPrompt: createImagePrompt,
+		createComp  : openai.createImage,
+		icon        : "image"
+	}
+}
 
 export const useCompStore = defineStore("counter", {
-  state: () => ({
-    completions: LocalStorage.getItem("completions") || {},
-    threads: {
-      main: {
-        messages: [],
-      },
-      ...(LocalStorage.getItem("threads") || {}),
-    } as Record<string, MessageThread>,
-    currentThread: "main",
-    userName: "You",
-  }),
-  getters: {
-    getAllCompletions(state) {
-      return state.completions;
-    },
-    getThread(state) {
-      return state.threads[state.currentThread];
-    },
-  },
-  actions: {
-    updateCache() {
-      LocalStorage.set("completions", this.completions);
-      LocalStorage.set("threads", this.threads);
-    },
-    clearCache() {
-      // clear whole local storage and reload
-      LocalStorage.clear();
-      location.reload();
-    },
-    clearThread() {
-      this.threads[this.currentThread].messages = [];
-      this.updateCache();
-    },
-    getByHash(hash: number) {
-      return this.completions[hash];
-    },
-    getCompletion(hash: number) {
-      const completion = this.getByHash(hash);
-      const choices = completion.choices;
-      const images = completion.data?.map((d: any) => d.url);
-      // const text = choices ? choices[0].text.trim() : undefined
-      const text = choices?.map((c: any) => c.text.trim());
-      console.warn("=> text:", text);
-      console.warn("=> images:", images);
-      return {
-        result: completion,
-        text: text,
-        images: images,
-        hash: hash,
-      };
-    },
-    async genTextCompletion(config: GenConfig): Promise<GenerationResult> {
-      const prompt = config.promptType.createPrompt(this.getThread.messages);
-      console.warn(prompt);
-      const hash = hashPrompt(prompt);
-      // if we already have a completion for this prompt, return it
-      if (!config.ignoreCache && this.completions[hash]) {
-        return {
-          ...this.getCompletion(hash),
-          cached: true,
-        };
-      }
-      // otherwise, generate a new completion
-      try {
-        const completion = await config.promptType.createComp(
-          {
-            ...config.promptType.config,
-            prompt: prompt,
-          },
-          options
-        );
+	state  : () => ({
+		completions  : LocalStorage.getItem("completions") || {},
+		threads      : {
+			main: {
+				messages: []
+			}, ...(LocalStorage.getItem("threads") || {})
+		} as Record<string, MessageThread>,
+		currentThread: "main",
+		userName     : "You"
+	}),
+	getters: {
+		getAllCompletions(state) {
+			return state.completions
+		},
+		getThread(state) {
+			return state.threads[state.currentThread]
+		}
+	},
+	actions: {
+		updateCache() {
+			LocalStorage.set("completions", this.completions)
+			LocalStorage.set("threads", this.threads)
+		},
+		clearCache() {
+			// clear whole local storage and reload
+			LocalStorage.clear()
+			location.reload()
+		},
+		clearThread() {
+			this.threads[this.currentThread].messages = []
+			this.updateCache()
+		},
+		getByHash(hash: number) {
+			return this.completions[hash]
+		},
+		getCompletion(hash: number) {
+			const completion = this.getByHash(hash)
+			const choices = completion.choices
+			const images = completion.data?.map((d: any) => d.url)
+			// const text = choices ? choices[0].text.trim() : undefined
+			const text = choices?.map((c: any) => c.text.trim())
+			console.warn("=> text:", text)
+			console.warn("=> images:", images)
+			return {
+				result: completion,
+				text  : text,
+				images: images,
+				hash  : hash
+			}
+		},
+		async genTextCompletion(config: GenConfig): Promise<GenerationResult> {
+			const prompt = config.promptType.createPrompt(this.getThread.messages)
+			console.warn(prompt)
+			const hash = hashPrompt(prompt)
+			// if we already have a completion for this prompt, return it
+			if (!config.ignoreCache && this.completions[hash]) {
+				return {
+					...this.getCompletion(hash),
+					cached: true
+				}
+			}
+			// otherwise, generate a new completion
+			try {
+				const completion = await config.promptType.createComp({
+					...config.promptType.config,
+					prompt: prompt
+				}, options)
 
-        if (!completion) throw new Error("No completion returned");
-        // then add it to the cache
-        this.completions[hash] = completion.data;
-        this.updateCache();
+				if (!completion) throw new Error("No completion returned")
+				// then add it to the cache
+				this.completions[hash] = completion.data
+				this.updateCache()
 
-        return {
-          ...this.getCompletion(hash),
-          cached: false,
-        };
-      } catch (error: any) {
-        let errorMsg = error;
-        if (error.response)
-          errorMsg = `${error.response.status} ${error.response.data}`;
-        return {
-          result: null,
-          cached: false,
-          hash: hash,
-          errorMsg: `Error: ${errorMsg}`,
-        };
-      }
-    },
-    pushMessage(message: TextMessage) {
-      if (message.id) {
-        // look back through the messages to see if we already have this message
-        // and update it if we do
-        const existing = this.getThread.messages.find(
-          (m) => m.id === message.id
-        );
-        if (existing !== undefined) {
-          for (const key in message) {
-            existing[key] = message[key];
-          }
-          existing.date = new Date();
-          console.log("Updated message", { ...existing });
-          this.updateCache();
-          return existing;
-        }
-      }
-      // otherwise, create uuid and push it
-      message.id = uuidv4();
-      this.getThread.messages.push(message);
-      console.log("Pushed message", { ...message });
-      this.updateCache();
-      return message;
-    },
-  },
-});
+				return {
+					...this.getCompletion(hash),
+					cached: false
+				}
+			} catch (error: any) {
+				let errorMsg = error
+				if (error.response) errorMsg = `${error.response.status} ${error.response.data}`
+				return {
+					result  : null,
+					cached  : false,
+					hash    : hash,
+					errorMsg: errorMsg
+				}
+			}
+		},
+		pushMessage(message: TextMessage) {
+			if (message.id) {
+				// look back through the messages to see if we already have this message
+				// and update it if we do
+				const existing = this.getThread.messages.find((m) => m.id === message.id)
+				if (existing !== undefined) {
+					for (const key in message) {
+						existing[key] = message[key]
+					}
+					existing.date = new Date()
+					console.log("Updated message", { ...existing })
+					this.updateCache()
+					return existing
+				}
+			}
+			// otherwise, create uuid and push it
+			message.id = uuidv4()
+			this.getThread.messages.push(message)
+			console.log("Pushed message", { ...message })
+			this.updateCache()
+			return message
+		}
+	}
+})
 
 export interface GenerationResult {
-  result: any;
-  text?: string[];
-  images?: string[];
-  hash: number;
-  cached: boolean;
-  errorMsg?: string;
+	result: any;
+	text?: string[];
+	images?: string[];
+	hash: number;
+	cached: boolean;
+	errorMsg?: string;
 }
 
 export const hashPrompt = (prompt: string): number => {
-  let hash = 0;
-  if (prompt.length === 0) return hash;
-  for (let i = 0; i < prompt.length; i++) {
-    const char = prompt.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash;
-  }
-  return hash;
-};
+	let hash = 0
+	if (prompt.length === 0) return hash
+	for (let i = 0; i < prompt.length; i++) {
+		const char = prompt.charCodeAt(i)
+		hash = (hash << 5) - hash + char
+		hash = hash & hash
+	}
+	return hash
+}

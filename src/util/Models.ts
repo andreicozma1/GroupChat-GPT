2388131476fa233bba934321d7830075c36efc86
Andrei Cozma <me@andreicozma.com>
2022-12-21 21:41:13 -0600

    Quick update/fix

--- src/util/Models.ts
+++ src/util/Models.ts
@@ -1,10 +1,10 @@
 export interface ColorConfig {
-  minLightness?: number;
-  maxLightness?: number;
-  minSaturation?: number;
-  maxSaturation?: number;
-  minHue?: number;
-  maxHue?: number;
-  minAlpha?: number;
-  maxAlpha?: number;
+	minLightness?: number;
+	maxLightness?: number;
+	minSaturation?: number;
+	maxSaturation?: number;
+	minHue?: number;
+	maxHue?: number;
+	minAlpha?: number;
+	maxAlpha?: number;
 }
